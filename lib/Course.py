56a1e3b89bd0426f70d2b1e75f432213a84beeb5
RegisLewis <regisaslewis@gmail.com>
2023-10-24 18:05:54 -0700

    object oriented scraping

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,12 @@
 class Course:
-    pass
+    def __init__(self, title="Test is broken", schedule="Test is broken", description="Test is broken"):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self):
+        output = ""
+        output += f"Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n"
+        output += "------------------"
+
+        return output
--- lib/Scraper.py
+++ lib/Scraper.py
@@ -5,4 +5,35 @@ import ipdb
 
 
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self):
+        doc = BeautifulSoup(
+            requests.get(
+                "http://learn-co-curriculum.github.io/site-for-scraping/courses"
+            ).text,
+            "html.parser"
+        )
+
+        return doc
+
+        
+    def get_courses(self):
+        return self.get_page().select(".post")
+    
+    def make_courses(self):
+        for n in self.get_courses():
+
+            title = n.select("h2")[0].text if n.select("h2") else ""
+            date = n.select(".date")[0].text if n.select(".date") else ""
+            description = n.select("p")[0].text if n.select("p") else ""
+
+            new_course = Course(title, date, description)
+            self.courses.append(new_course)
+
+        return self.courses
+
+    def print_courses(self):
+        for n in self.make_courses():
+            print(n)
